84147069568ba1d52093ab25b6f3053358a95c3d
Mihai-Tudor Popescu <external.popescu_mihai-tudor@allianzdirect.de>
2023-11-16 19:18:16 +0200

    Updated Swagger, implemented flows

--- src/main/java/com/univbuc/eventreservation/configuration/SwaggerConfig.java
+++ src/main/java/com/univbuc/eventreservation/configuration/SwaggerConfig.java
@@ -1,17 +1,43 @@
 package com.univbuc.eventreservation.configuration;
 
-import org.springdoc.core.GroupedOpenApi;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${eventReservation.openapi.dev-url}")
+    private String devUrl;
+
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("spring-boot-api")
-                .pathsToMatch("/**")
-                .build();
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("dev@example.com");
+        contact.setName("Dev");
+        contact.setUrl("https://www.example.com");
+
+        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("Event ticket reservation API")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes endpoints to reserve event tickets.").termsOfService("https://www.example.com/terms")
+                .license(mitLicense);
+
+        return new OpenAPI().info(info).servers(List.of(devServer));
     }
 }
