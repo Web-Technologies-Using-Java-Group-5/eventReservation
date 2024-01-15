6bfd60edf7d63df36761ebb439136be5252ddbab
Mihai-Tudor Popescu <external.popescu_mihai-tudor@allianzdirect.de>
2024-01-15 15:05:37 +0200

    Added integration tests

--- src/test/java/com/univbuc/eventreservation/admin/controllers/AdminFlowIntegrationTest.java
+++ src/test/java/com/univbuc/eventreservation/admin/controllers/AdminFlowIntegrationTest.java
@@ -2,18 +2,14 @@ package com.univbuc.eventreservation.admin.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.univbuc.eventreservation.admin.model.Event;
-import com.univbuc.eventreservation.admin.services.AdminService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-import static org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,20 +24,17 @@ class AdminFlowIntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
-    AdminService adminService;
-
     ObjectMapper mapper = new ObjectMapper();
 
     @Test
+    @Transactional
     void addEvent() throws Exception {
         // Arrange
-        when(adminService.addEvent(INPUT_EVENT)).thenReturn(RESULTED_EVENT);
 
         // Act & Assert
         mockMvc.perform(post("/admin/events").content(
                         mapper.writerWithDefaultPrettyPrinter().writeValueAsString(INPUT_EVENT)
-                ).accept(MediaType.APPLICATION_JSON))
+                ).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(RESULTED_EVENT)));
     }
